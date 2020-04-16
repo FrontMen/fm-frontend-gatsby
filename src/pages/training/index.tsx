a44e05d5a538484bacc7725d2b5c8733ae1a17a4
Randynamic <randy@randynamic.com>
2020-04-16 14:22:38 +0200

    first setup for training page

--- src/pages/training/index.tsx
+++ src/pages/training/index.tsx
@@ -4,46 +4,39 @@ import * as React from 'react';
 // eslint-disable-next-line import/no-unresolved
 import { TrainingPageQuery } from '../../../types/graphql-types';
 import Layout from '../../components/layout';
-import { SectionContainer } from '../../components/sectionContainer';
 import SEO from '../../components/seo';
+import ContentModules from '../../components/contentModules';
+import { DebugData } from '../../components/debugData';
 
 type Props = {
   data: TrainingPageQuery;
 };
 
 const TrainingPage: React.FC<Props> = ({ data }: Props) => {
+  const layout = data.contentfulLayout;
+  const title = layout?.title || 'Training';
+
   return (
     <Layout>
-      <SEO title="Training" />
-      <SectionContainer>
-        <h1>Training</h1>
-      </SectionContainer>
+      <SEO title={title} />
+      {layout?.contentModules && (
+        <ContentModules contentModules={layout.contentModules} />
+      )}
+      <DebugData data={data} />
     </Layout>
   );
 };
 
 export const query = graphql`
-  query TrainingPage {
-    contentfulLayout(slug: { eq: "Training" }) {
+  query TrainingPage($lang: String = "en-US") {
+    contentfulLayout(node_locale: { eq: $lang }, slug: { eq: "training" }) {
       slug
       title
-      #   contentModules {
-      #     ... on ContentfulLayoutHeroImage {
-      #       id
-      #       headline
-      #       backgroundImage {
-      #         fluid(maxWidth: 1200) {
-      #           src
-      #         }
-      #       }
-      #     }
-      #     ... on ContentfulLayoutCallToAction {
-      #       id
-      #       title
-      #       url
-      #       label
-      #     }
-      #   }
+      contentModules {
+        ... on ContentfulLayoutCopy {
+          ...ContentfulLayoutCopyFragment
+        }
+      }
     }
   }
 `;
